020ede3ba772d167e5d116c40feb384f54732f57
ConorG1247 <conorgoddard1247@gmail.com>
2022-07-28 13:28:14 +0100

    removed temp data

--- bok-frontend/components/ReadingList.js
+++ bok-frontend/components/ReadingList.js
@@ -1,4 +1,3 @@
-import {useState} from "react";
 import styles from "../styles/BookItem.module.css";
 import { Button } from "@mui/material";
 import { useRouter } from "next/router";
@@ -9,22 +8,6 @@ function ReadingList({ readingList }) {
   const handleClick = (route) => {
     router.push("list/?id=" + route)
   }
-
-
-  const data = [
-    {
-      id: 1,
-      listTitle: "History",
-      image:
-        "https://www.creativindie.com/wp-content/uploads/2012/07/stock-image-site-pinterest-graphic.jpg",
-    },
-    {
-      id: 2,
-      listTitle: "Sci-fi",
-      image:
-        "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/action-thriller-book-cover-design-template-3675ae3e3ac7ee095fc793ab61b812cc_screen.jpg?ts=1637008457",
-    },
-  ];
 
   console.log(readingList);
 
