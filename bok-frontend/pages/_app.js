c08c7879c5d4349384fe92caf8e10adedd611d5f
Khulvinder Mall <khullymall@gmail.com>
2022-07-28 10:45:32 +0100

    adding authentication using auth0. Login and logout working.

--- bok-frontend/pages/_app.js
+++ bok-frontend/pages/_app.js
@@ -1,50 +1,48 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import Head from 'next/head';
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import { CacheProvider } from '@emotion/react';
-import theme from './src/theme';
-import createEmotionCache from './src/createEmotionCache';
-import Layout from '../components/Layout';
-import {useRouter} from "next/router"
-  
-// Client-side cache shared for the whole session 
-// of the user in the browser.
-  
-const clientSideEmotionCache = createEmotionCache();
-  
-export default function MyApp(props) {
-    const { Component, emotionCache = 
-        clientSideEmotionCache, pageProps } = props;
+import * as React from "react";
+import PropTypes from "prop-types";
+import Head from "next/head";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { CacheProvider } from "@emotion/react";
+import theme from "./src/theme";
+import createEmotionCache from "./src/createEmotionCache";
+import Layout from "../components/Layout";
+import { useRouter } from "next/router";
+import { UserProvider } from "@auth0/nextjs-auth0";
 
-    const router = useRouter();
-    const showHeader = router.pathname === '/logIn' ? false : true;
-  
-    return (
-         
-        <CacheProvider value={emotionCache}>
-            <Head>
-                <meta name="viewport" 
-                    content="initial-scale=1, width=device-width" />
-            </Head>
-            <ThemeProvider theme={theme}>
-                  
-                {/* CssBaseline kickstart an elegant, 
+// Client-side cache shared for the whole session
+// of the user in the browser.
+
+const clientSideEmotionCache = createEmotionCache();
+
+export default function MyApp(props) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  const router = useRouter();
+  const showHeader = router.pathname === "/logIn" ? false : true;
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, 
                 consistent, and simple baseline to
                 build upon. */}
-                  
-                <CssBaseline />
-                {showHeader && <Layout/>}  
-                <Component {...pageProps} />
-            </ThemeProvider>
-        </CacheProvider>
-        
-    );
+
+        <CssBaseline />
+        <UserProvider>
+          {showHeader && <Layout />}
+          <Component {...pageProps} />
+        </UserProvider>
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
-  
+
 MyApp.propTypes = {
-    Component: PropTypes.elementType.isRequired,
-    emotionCache: PropTypes.object,
-    pageProps: PropTypes.object.isRequired,
+  Component: PropTypes.elementType.isRequired,
+  emotionCache: PropTypes.object,
+  pageProps: PropTypes.object.isRequired,
 };
--- bok-frontend/pages/logIn.js
+++ bok-frontend/pages/logIn.js
@@ -1,18 +1,19 @@
-import React from 'react'
-import Image from 'next/image'
-import Button from '@mui/material/Button';
-import styles from "../styles/Login.module.css"
+import React from "react";
+import Image from "next/image";
+import Button from "@mui/material/Button";
+import styles from "../styles/Login.module.css";
 
 function login() {
   return (
     <div className={styles.loginContainer}>
-    <Image src="/BokLogo.png" width="100" height="60"></Image>
-    <div className={styles.buttonContainer}>
-      <Button variant="filled">Sign Up</Button>
-      <Button variant="filled">Login</Button>
+      <Image src="/BokLogo.png" width="100" height="60"></Image>
+      <div className={styles.buttonContainer}>
+        <a href="/api/auth/login">Signup</a>
+
+        <a href="/api/auth/login">Login</a>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default login
+export default login;
