b8944f69312829337849cbc96d3fc0376672ca1e
Paul2071 <101050756+Paul2071@users.noreply.github.com>
2022-08-02 12:06:42 +0100

    added recharts and edited profile page

--- bok-frontend/pages/profile.js
+++ bok-frontend/pages/profile.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { useUser } from "@auth0/nextjs-auth0";
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import Rating from '@mui/material/Rating';
+import Typography from '@mui/material/Typography';
+import FavouriteSubjectPieChart from "../components/FavouriteSubjectPieChart";
 
 //Displays the users profile information
 
 export default function user() {
-  const { user, error, isLoading } = useUser();
+   const { user, error, isLoading } = useUser();
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
@@ -13,10 +16,25 @@ export default function user() {
   return (
     user && (
       <div>
-        <img src={user.picture} alt={user.name} />
+      <div>
+        <img src={user?.picture} alt={user.name} />
         <h2>{user.name}</h2>
         <p>{user.email}</p>
-        <p>{user.sub}</p>
+      </div>
+      <div>
+        <h2>Your Statistic</h2>
+        <Typography component="legend">Average Star Rating</Typography>
+      <Rating
+        readOnly
+        name="simple-controlled"
+        value= {3}
+      />
+      <h2>Total Books on Lists:</h2>
+      <p>5</p>
+      </div>
+      <div>
+        <FavouriteSubjectPieChart />
+      </div>
       </div>
     )
   );
