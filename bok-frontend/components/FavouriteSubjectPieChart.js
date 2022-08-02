0441b2632adebdf5f6c6ff6257d7309c8b4cde65
Khulvinder Mall <khullymall@gmail.com>
2022-08-02 14:15:38 +0100

    Adding favourite subject pie chart. No styling added

--- bok-frontend/components/FavouriteSubjectPieChart.js
+++ bok-frontend/components/FavouriteSubjectPieChart.js
@@ -1,29 +1,109 @@
-import React from "react";
-import { PieChart, Pie, Legend, Tooltip } from "recharts";
+import React, { useCallback, useState } from "react";
+import { PieChart, Pie, Sector } from "recharts";
 
-const data01 = [
-  { name: "Group A", value: 400 },
-  { name: "Group B", value: 300 },
-  { name: "Group C", value: 300 },
-  { name: "Group D", value: 200 },
-  { name: "Group E", value: 278 },
-  { name: "Group F", value: 189 }
+const data = [
+  { name: "Fantasy", value: 7 },
+  { name: "Sci-Fi", value: 11 },
+  { name: "Horror", value: 23 },
+  { name: "Comedy", value: 15 },
 ];
 
-export default function FavouriteSubjectPieChart() {
+const renderActiveShape = (props) => {
+  const RADIAN = Math.PI / 180;
+  const {
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    startAngle,
+    endAngle,
+    fill,
+    payload,
+    percent,
+    value,
+  } = props;
+  const sin = Math.sin(-RADIAN * midAngle);
+  const cos = Math.cos(-RADIAN * midAngle);
+  const sx = cx + (outerRadius + 10) * cos;
+  const sy = cy + (outerRadius + 10) * sin;
+  const mx = cx + (outerRadius + 30) * cos;
+  const my = cy + (outerRadius + 30) * sin;
+  const ex = mx + (cos >= 0 ? 1 : -1) * 22;
+  const ey = my;
+  const textAnchor = cos >= 0 ? "start" : "end";
+
   return (
-    <PieChart width={1000} height={400}>
+    <g>
+      <text x={cx} y={cy} dy={8} textAnchor="middle" fill={fill}>
+        {payload.name}
+      </text>
+      <Sector
+        cx={cx}
+        cy={cy}
+        innerRadius={innerRadius}
+        outerRadius={outerRadius}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        fill={fill}
+      />
+      <Sector
+        cx={cx}
+        cy={cy}
+        startAngle={startAngle}
+        endAngle={endAngle}
+        innerRadius={outerRadius + 6}
+        outerRadius={outerRadius + 10}
+        fill={fill}
+      />
+      <path
+        d={`M${sx},${sy}L${mx},${my}L${ex},${ey}`}
+        stroke={fill}
+        fill="none"
+      />
+      <circle cx={ex} cy={ey} r={2} fill={fill} stroke="none" />
+      <text
+        x={ex + (cos >= 0 ? 1 : -1) * 12}
+        y={ey}
+        textAnchor={textAnchor}
+        fill="#333"
+      >{`${value}`}</text>
+      <text
+        x={ex + (cos >= 0 ? 1 : -1) * 12}
+        y={ey}
+        dy={18}
+        textAnchor={textAnchor}
+        fill="#999"
+      >
+        {`(${(percent * 100).toFixed(2)}%)`}
+      </text>
+    </g>
+  );
+};
+
+export default function App() {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const onPieEnter = useCallback(
+    (_, index) => {
+      setActiveIndex(index);
+    },
+    [setActiveIndex]
+  );
+
+  return (
+    <PieChart width={400} height={400}>
       <Pie
-        dataKey="value"
-        isAnimationActive={true}
-        data={data01}
+        activeIndex={activeIndex}
+        activeShape={renderActiveShape}
+        data={data}
         cx={200}
-        cy={200}
+        cy={125}
+        innerRadius={60}
         outerRadius={80}
         fill="#8884d8"
-        label
+        dataKey="value"
+        onMouseEnter={onPieEnter}
       />
-      <Tooltip />
     </PieChart>
   );
 }
--- bok-frontend/pages/profile.js
+++ bok-frontend/pages/profile.js
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
 import { useUser } from "@auth0/nextjs-auth0";
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
-import Rating from '@mui/material/Rating';
-import Typography from '@mui/material/Typography';
+import Rating from "@mui/material/Rating";
+import Typography from "@mui/material/Typography";
 import FavouriteSubjectPieChart from "../components/FavouriteSubjectPieChart";
 
 //Displays the users profile information
 
 export default function user() {
-   const { user, error, isLoading } = useUser();
+  const { user, error, isLoading } = useUser();
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>{error.message}</div>;
@@ -16,25 +16,22 @@ export default function user() {
   return (
     user && (
       <div>
-      <div>
-        <img src={user?.picture} alt={user.name} />
-        <h2>{user.name}</h2>
-        <p>{user.email}</p>
-      </div>
-      <div>
-        <h2>Your Statistic</h2>
-        <Typography component="legend">Average Star Rating</Typography>
-      <Rating
-        readOnly
-        name="simple-controlled"
-        value= {3}
-      />
-      <h2>Total Books on Lists:</h2>
-      <p>5</p>
-      </div>
-      <div>
-        <FavouriteSubjectPieChart />
-      </div>
+        <div>
+          <img src={user?.picture} alt={user.name} />
+          <h2>{user.name}</h2>
+          <p>{user.email}</p>
+        </div>
+        <div>
+          <h2>Your Statistic</h2>
+          <Typography component="legend">Average Star Rating</Typography>
+          <Rating readOnly name="simple-controlled" value={4.5} />
+          <h2>Total Books on Lists:</h2>
+          <p>5</p>
+        </div>
+        <div>
+          <h3>Favourite Subject</h3>
+          <FavouriteSubjectPieChart />
+        </div>
       </div>
     )
   );
