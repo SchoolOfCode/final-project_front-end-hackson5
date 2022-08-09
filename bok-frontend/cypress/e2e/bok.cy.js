36b995a296ed892f22a9d8320d488a06888db240
Rory Hogan <roryhogan@mail.com>
2022-08-09 15:01:29 +0100

    cypress testing up to home page

--- bok-frontend/cypress/e2e/bok.cy.js
+++ bok-frontend/cypress/e2e/bok.cy.js
@@ -18,8 +18,30 @@ Cypress.Commands.add("login", (overrides = {}) => {
   cy.request(options);
 });
 
-describe("empty spec", () => {
-  it("passes", () => {
+describe("End to End testing for BOK app", () => {
+  it("Surprise me page test: user input and search", () => {
     cy.visit("http://localhost:3000/surpriseme");
-  });
+    cy.get('.SurpriseMe_search__nbXOD').type('horror');
+    cy.get('.MuiButton-root').click();
+    cy.wait(6000);
+    cy.contains('Add to list');
+    cy.get('.NavBar_logo__MYO60').click();
+    cy.wait(6000);
+    });
+
+  it("Homepage: stats graph, search bar and navigation", () => {
+    cy.get(':nth-child(6) > .recharts-rectangle').click();
+    cy.contains('Jane Austen');
+    cy.get(':nth-child(2) > .recharts-rectangle').click();
+    cy.contains('Stephen King');
+    cy.get(':nth-child(7) > .recharts-rectangle').click();
+    cy.contains('George R.R. Martin');
+    cy.get('.Home_FavAutChart__VsCR_ > .MuiButton-root');
+    cy.get('.Home_container__bCOhY > :nth-child(4)');
+    cy.get('.MuiFormControl-root').type('Harry');
+    cy.get('[data-testid="SearchIcon"]').click();
+    cy.wait(6000);
+
+        });
+
 });
